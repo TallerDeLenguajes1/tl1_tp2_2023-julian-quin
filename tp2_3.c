579087de091185ed35c12e1691bf4bd108bf3fd8
julian <jnsoretniuq1998@gmail.com>
2023-03-25 17:57:14 -0300

    punto 3 usando punteros, corregido

--- tp2_3.c
+++ tp2_3.c
@@ -7,19 +7,18 @@
 int main(void)
 {
     double mt[N][M], *puntero;
-    int k = 0, x = 0;
     puntero = &mt[0][0];
     srand(time(NULL));
     for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < M; j++)
         {
-            *(puntero + k) = 1 + rand() % 100;
-            printf(" |%.2lf| ", *(puntero + x));
-            k++;
-            x++;
+            *(puntero) = 1 + rand() % 100;
+            printf(" |%.2lf| ", *(puntero));
+            puntero++;
         }
         printf("\n");
     }
+
     return 0;
 }
